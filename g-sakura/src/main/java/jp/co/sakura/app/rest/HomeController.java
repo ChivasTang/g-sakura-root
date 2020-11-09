4dc70df317268ee96059fd57c890f06db2f232be
ChivasTang <tangzh1983@gmail.com>
2020-11-09 22:51:32 +0900

    20201108

--- g-sakura/src/main/java/jp/co/sakura/app/rest/HomeController.java
+++ g-sakura/src/main/java/jp/co/sakura/app/rest/HomeController.java
@@ -1,6 +1,5 @@
 package jp.co.sakura.app.rest;
 
-import jp.co.sakura.core.domain.dto.RegistrationDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -21,18 +20,16 @@ public class HomeController {
     }
 
     @PostMapping("/login")
-    public ModelAndView login(HttpServletRequest request, HttpServletResponse response, Model model, Locale locale,@RequestParam("returnUrl") String returnUrl) {
+    public ModelAndView login(HttpServletRequest request, HttpServletResponse response, Model model, Locale locale) {
         ModelAndView mv = new ModelAndView("login");
         mv.addObject("locale", "ja");
-        mv.addObject("returnUrl",returnUrl);
         return mv;
     }
 
     @PostMapping("/registration")
-    public ModelAndView registration(HttpServletRequest request, HttpServletResponse response, Model model, Locale locale, @ModelAttribute("registrationDTO") RegistrationDTO registrationDTO, @RequestParam("returnUrl") String returnUrl) {
+    public ModelAndView registration(HttpServletRequest request, HttpServletResponse response, Model model, Locale locale) {
         ModelAndView mv = new ModelAndView("registration");
         mv.addObject("locale", "ja");
-        mv.addObject("returnUrl",returnUrl);
         return mv;
     }
 }
